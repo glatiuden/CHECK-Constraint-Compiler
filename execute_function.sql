d05287609edac0b4295df4b899b103e2b7807c27
Jian Wei <kongjianwei@outlook.com>
2022-03-28 21:55:19 +0800

    Add more test cases

--- execute_function.sql
+++ execute_function.sql
@@ -1,15 +1,144 @@
+-- VALID: Trivial Example - any customer aged below 21 downloading 'Domainer' will trigger the function
+-- EXPECTED RESULT: true
 Select add_constraint(
-'r21',
-'
-SELECT c.customerid
-FROM customers c NATURAL JOIN downloads d
-WHERE d.name = ''Domainer'' AND c.age < 21
-',
-'EXISTS',
-'AFTER',
-'INSERT OR UPDATE',
-'DEFERRABLE',
-'INITIALLY DEFERRED',
-'customers, downloads',
-'An underaged customer cannot download Domainer'
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'EXISTS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DEFERRED',
+    'customers, downloads',
+    'An underaged customer cannot download Domainer'
+);
+
+
+-- INVALID: invalid SQL query as condition
+-- EXPECTED RESULT: Invalid condition entered, make sure it is a valid SQL query!
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < ABC
+    ',
+    'EXISTS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DEFERRED',
+    'customers, downloads',
+    'An underaged customer cannot download Domainer'
+);
+
+-- INVALID: EXIST sytax, only [EXISTS / NOT EXISTS] (case insensitive)
+-- EXPECTED RESULT: "HAS" is not a valid argument, either input "EXISTS" or "NOT EXISTS" (case insensitive)
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'HAS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DEFERRED',
+    'customers, downloads',
+    'An underaged customer cannot download Domainer'
+);
+
+-- INVALID: WHEN syntax, only [BEFORE | AFTER | INSTEAD OF] (case insensitive)
+-- EXPECTED RESULT: PSQL default error message
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'EXISTS',
+    'IN BETWEEN',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DEFERRED',
+    'customers, downloads',
+    'An underaged customer cannot download Domainer'
+);
+
+-- INVALID: DEFER syntax, only [DEFERRABLE | NOT DEFERRABLE] (case insensitive)
+-- EXPECTED RESULT: PSQL default error message
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'EXISTS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DELAYABLE',
+    'INITIALLY DEFERRED',
+    'customers, downloads',
+    'An underaged customer cannot download Domainer'
+);
+
+-- INVALID: TIMING syntax, only [INITIALLY IMMEDIATE | INITIALLY DEFERRED] (case insensitive)
+-- EXPECTED RESULT: PSQL default error message
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'EXISTS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DELAYED',
+    'customers, downloads',
+    'An underaged customer cannot download Domainer'
+);
+
+-- INVALID: No table entered by user
+-- EXPECTED RESULT: No table input detected, please input at least one existing table for the trigger to be applied to!
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'EXISTS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DEFERRED',
+    '',
+    'An underaged customer cannot download Domainer'
+);
+
+-- INVALID: Table entered by user does not exists
+-- EXPECTED RESULT: Table name "downloadsssss" does not exists!
+Select add_constraint(
+    'r21',
+    '
+    SELECT c.customerid
+    FROM customers c NATURAL JOIN downloads d
+    WHERE d.name = ''Domainer'' AND c.age < 21
+    ',
+    'EXISTS',
+    'AFTER',
+    'INSERT OR UPDATE',
+    'DEFERRABLE',
+    'INITIALLY DEFERRED',
+    'customers, downloadsssss',
+    'An underaged customer cannot download Domainer'
 );
--- test_cases.sql
+++ test_cases.sql
@@ -1,15 +1,28 @@
 -- Run these one by one
 TRUNCATE customers;
 
-INSERT INTO customers(customerid, age, downloadsid) VALUES (1, 26, 1); -- valid
-INSERT INTO customers(customerid, age, downloadsid) VALUES (2, 20, 1); -- invalid
-INSERT INTO customers(customerid, age, downloadsid) VALUES (2, 20, 2); -- valid
-UPDATE downloads SET name = 'Domainer' WHERE downloadsid = 2; -- invalid
+-- VALID: new customer age 26 downloading 'Domainer'
+INSERT INTO customers(customerid, age, downloadsid) VALUES (1, 26, 1);
 
-UPDATE customers SET age = 36 WHERE customerid = 1; -- valid
-UPDATE customers SET age = 20 WHERE customerid = 1; -- invalid
+-- INVALID: new customer aged 20 downloading 'Domainer'
+INSERT INTO customers(customerid, age, downloadsid) VALUES (2, 20, 1);
+
+-- VALID: new customer aged 20 downloading 'Terminator'
+INSERT INTO customers(customerid, age, downloadsid) VALUES (2, 20, 2);
+
+-- INVALID: renaming 'Terminator' to 'Domainer' where a customer under age 21 has downloaded
+UPDATE downloads SET name = 'Domainer' WHERE downloadsid = 2;
+
+-- VALID: set customer that downlaoded 'Domainer' before age to 36
+UPDATE customers SET age = 36 WHERE customerid = 1;
+
+-- INVALID: set customer that downloaded 'Domainer' before age to 20
+UPDATE customers SET age = 20 WHERE customerid = 1;
+
+-- INVALID: update underage customer to download 'Domainer'
 UPDATE customers SET downloadsid = 1 WHERE customerid = 2; -- invalid
 
+-- VALID: set underage customer to 30 and download 'Domainer'
 -- The next 2 query has to execute together
 UPDATE customers SET age = 30 WHERE customerid = 2; -- valid
 UPDATE customers SET downloadsid = 1 WHERE customerid = 2; -- valid
